a76049c02c29ad6a04eb79254928953133dc4edd
sunahkim <ksa4573@gmail.com>
2019-08-16 22:36:03 +0900

    collab코드 커밋

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    Rigidbody2D rb;
+    public float speed;
+    public float jumpPower;
+    bool isJumping;
+    public bool canMove;
+    GameObject head;
+    public GameObject targetPoint;
+    
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+        isJumping = false;
+        canMove = true;
+        head = transform.Find("Head").gameObject;
+    }
+    
+    void Update()
+    {
+        if (!canMove)
+        {
+            //return;
+        }
+
+        float inputX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(inputX * speed, 0f);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            if (!isJumping)
+            {
+                isJumping = true;
+                rb.AddForce(new Vector2(0, jumpPower));
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Land")
+        {
+            isJumping = false;
+        }
+    }
+}
