58663e0a2dd6d440750823256d9a45b56bbc075b
genius5375 <52522719+genius5375@users.noreply.github.com>
2019-08-17 00:19:03 +0900

    헤드 닿았을 때 호출 함수,  hp 감소 함수 만들었습니다

---
+++ Assets/MapLogic/StageClear.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageClear : MonoBehaviour
+{ private bool IsClear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsClear) Debug.Log("Hello");
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Head") IsClear = true;
+    }
+}
