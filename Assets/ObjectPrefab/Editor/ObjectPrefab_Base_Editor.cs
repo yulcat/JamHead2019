792eda38ca61a8ab066c182dac2274fac52f6780
sunahkim <ksa4573@gmail.com>
2019-08-16 21:48:44 +0900

    실수한것+objec처리

---
+++ Assets/ObjectPrefab/Editor/ObjectPrefab_Base_Editor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(ObjectPrefab_Base), true)]
+public class ObjectPrefab_Base_Editor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        ObjectPrefab_Base ObjectPrefab = target as ObjectPrefab_Base;
+        EditorGUILayout.LabelField("", GUI.skin.horizontalSlider);
+
+        EditorGUILayout.Space();
+        EditorGUILayout.BeginHorizontal();
+        if (GUILayout.Button("작동"))
+            ObjectPrefab.SetState(true);
+        if (GUILayout.Button("비활성화"))
+            ObjectPrefab.SetState(false);
+        EditorGUILayout.EndHorizontal();
+
+        //EditorGUI.BeginDisabledGroup();
+        //EditorGUI.EndDisabledGroup();
+
+    }
+    private void OnSceneGUI()
+    {
+        ObjectPrefab_Base ObjectPrefab = target as ObjectPrefab_Base;
+
+        Handles.color = Color.green;
+        foreach (var iter in ObjectPrefab.LinkObjects)
+        {
+            Handles.DrawDottedLine(ObjectPrefab.transform.position
+                , iter.transform.position,0.5f);
+        }
+
+    }
+
+
+}
