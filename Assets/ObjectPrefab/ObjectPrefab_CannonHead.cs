25f271216ffce4d2ef37603dd3a8cfd6f367078b
sunahkim <ksa4573@gmail.com>
2019-08-17 16:38:41 +0900

    공유

--- Assets/ObjectPrefab/ObjectPrefab_CannonHead.cs
+++ Assets/ObjectPrefab/ObjectPrefab_CannonHead.cs
@@ -6,14 +6,12 @@ public class ObjectPrefab_CannonHead : ObjectPrefab_Base
 {
     public float shootForce = 20;
     public Vector2 direction = Vector2.right;
-    //PlayerController playerController;
     [SerializeField] protected Rigidbody2D BulletObject;
 
     protected override void Start()
     {
         base.Start();
         direction.Normalize();
-        //playerController = transform.parent.GetComponent<PlayerController>();
     }
 
 
@@ -23,7 +21,7 @@ public class ObjectPrefab_CannonHead : ObjectPrefab_Base
         {
             Vector2 tempDirection = direction;
             if (transform.lossyScale.x < 0)
-                tempDirection.x *= -1;
+                tempDirection.x *= -1;  
             BulletObject.velocity = shootForce * tempDirection;
             BulletObject = null;
             return false;
