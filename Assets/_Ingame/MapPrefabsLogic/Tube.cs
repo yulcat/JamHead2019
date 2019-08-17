3adedb38045e18675e4fe2f911a6ddbee6e8abed
genius5375 <52522719+genius5375@users.noreply.github.com>
2019-08-17 15:12:53 +0900

    진공튜브 살짝 코드 건드리기

---
+++ Assets/_Ingame/MapPrefabsLogic/Tube.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tube : ObjectPrefab_Action_Base
+{
+    public bool IsEnter;
+    public List<Vector2> MovePath;
+    private int MoveIndex;
+    public float Velocity;
+    public GameObject Target;
+    public Vector2 OriPos;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Moving();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Head")
+        {
+            IsEnter = true; Target = collision.gameObject; OriPos = Target.transform.position;
+        }
+    }
+    private void Moving()
+    {  if (IsEnter != true) return;
+        Vector2 CurPos = Target.transform.position;
+        if (MoveIndex == MovePath.Count) return;
+        if (MoveIndex < MovePath.Count)
+        { float step = Velocity * Time.deltaTime;
+            Target.transform.position = Vector2.MoveTowards(CurPos, OriPos + MovePath[MoveIndex], step);
+            
+            
+        }
+        if (Vector2.Distance(Target.transform.position, OriPos + MovePath[MoveIndex]) == 0) MoveIndex++;
+    }
+}
