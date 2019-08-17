24f75971cdb62e2c30149f778b3c5b0d4a854fea
genius5375 <52522719+genius5375@users.noreply.github.com>
2019-08-17 23:16:14 +0900

    씬1부터 씬4까지 자연스럽게 넘어가도록 구현했습니다

--- Assets/_Ingame/MapPrefabsLogic/StageClear.cs
+++ Assets/_Ingame/MapPrefabsLogic/StageClear.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StageClear : ObjectPrefab_Action_Base
 { private bool IsClear;
     public string WinTarget="Head";
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +16,8 @@ public class StageClear : ObjectPrefab_Action_Base
     // Update is called once per frame
     void Update()
     {
-        if (IsClear) Debug.Log("You Clear");
+        if (IsClear) { Debug.Log("You Clear");
+            if(SceneManager.GetActiveScene().buildIndex<SceneManager.sceneCountInBuildSettings)SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); }
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
