918f7d73a78ebb90437ef1446ef12cd4934c5fe7
Jiho <sjhkr2009@naver.com>
2019-08-18 13:01:55 +0900

    클리어 효과음 추가
    (+ 배경음 씬 변경시에도 지속)

--- Assets/Ingame/MapPrefabsLogic/StageClear.cs
+++ Assets/Ingame/MapPrefabsLogic/StageClear.cs
@@ -6,11 +6,12 @@ using UnityEngine.SceneManagement;
 public class StageClear : ObjectPrefab_Action_Base
 { private bool IsClear;
     public string WinTarget="Head";
+    AudioSource clearFX;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        clearFX = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -21,7 +22,11 @@ public class StageClear : ObjectPrefab_Action_Base
     }
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.tag == WinTarget) IsClear = true;
+        if (collision.gameObject.tag == WinTarget)
+        {
+            IsClear = true;
+            clearFX.Play();
+        }
     }
 
 
