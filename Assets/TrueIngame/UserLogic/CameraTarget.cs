4183634eea5fee16b66d5a9666a5bc3f38c6e933
Jiho <sjhkr2009@naver.com>
2019-08-18 12:44:21 +0900

    재시작 시 효과음 추가

--- Assets/TrueIngame/UserLogic/CameraTarget.cs
+++ Assets/TrueIngame/UserLogic/CameraTarget.cs
@@ -4,17 +4,23 @@ using UnityEngine.SceneManagement;
 public class CameraTarget : MonoBehaviour
 {
     private GameObject target;
+    [SerializeField] GameObject MenuSelection;
 
     void Start()
     {
         var withHead = GameObject.FindWithTag("GetHead");
         target = withHead ? withHead : GameObject.FindWithTag("Head");
+        MenuSelection.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10f);
-        if (Input.GetKeyDown(KeyCode.R)) SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            MenuSelection.SetActive(true);
+        }
     }
 }
